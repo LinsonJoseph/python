3dc4e55debd9a819acda2224d089babd5fe54be0
Linson Joseph <linsonjosephDev@gmail.com>
2020-02-26 02:39:31 +0530

    Add files via upload
    This code compare two input strings and informs if they are identical or not. I also demonstrates the use of logical operator ==. 
    
    The code execution samples are as below:
    ----------------------------------------------------------------
    Enter a string: Coffee is for the sleepy
    Enter another string: Coffee is not for every sleepy head
    Strings are not identical
    ----------------------------------------------------------------
    Enter a string: I am 18
    Enter another string: I am 18 indeed
    Strings are not identical
    ----------------------------------------------------------------
    Enter a string: Jesus is LORD and Savior
    Enter another string: Jesus is LORD and Savior
    Identical strings entered!
    ----------------------------------------------------------------

---
+++ IdentifyIfTwoInputStringsAreIdentical.py
@@ -0,0 +1,7 @@
+#Compare two strings
+str1 = input("Enter a string: ")
+str2 = input("Enter another string: ")
+if str1 == str2:
+    print("Identical strings entered!")
+else:
+    print("Strings are not identical")
