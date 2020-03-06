37ce27868f7ae4be9e6c6b97b526448665ae4099
Linson Joseph <linsonjosephDev@gmail.com>
2020-03-07 04:45:23 +0530

    fetching Python version and format on strings
    format method is used on strings, please notice the different ways you may use it. 
    
    A sample output of the code execution is given below:
    ---------------------------------------------------------------
    Hello, Programmer! You are running Python version 3.8.2
    Hello, Programmer! You are running Python version 3.8.2
    ---------------------------------------------------------------

--- import_platform.py
+++ import_platform.py
@@ -1,10 +1,17 @@
+#import platform to help print the version of Python the user is running
 import platform
-print('You are running Python version {}'.format(platform.python_version()))
 
-world = 'World!'; hello = 'Hello, {}'.format(world)
-print(hello)
+#------------------------------------------------------------------------------------------------
+#You may assign multiple variable in one line (however, it makes reading the code tough)
+programmer = 'Programmer!'; welcome_programmer = 'Hello, {}'.format(programmer)
+print(welcome_programmer, 'You are running Python version {}'.format(platform.python_version()))
 
+#------------------------------------------------------------------------------------------------
+#Another way of writing this code in Python 3.6 and above is: 
+programmer = ' Programmer'
+greet_programmer = f'Hello,{programmer}'
 
-world = 'World!'
-hello = 'Hello, {}'.format(world)
-print(hello)
+version = platform.python_version()
+print(welcome_programmer, f'You are running Python version {version}')
+
+#------------------------------------------------------------------------------------------------
