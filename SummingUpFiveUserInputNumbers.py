622e575390bda3269e33a0611058a53d4f189d45
Linson Joseph <linsonjosephDev@gmail.com>
2020-03-08 07:39:33 +0530

    Sum of five numbers with input validation
    Use of while, except and continue demonstrated. A sample of the output of this code is:
    
    --------------------------------------------------------------------------------------------
    Input an integer(1): 2.3
    Input an integer(1): 5.6
    Input an integer(1): 7
    Sum of current and previous input(s) is:  7
    Input an integer(2): 4.2
    Input an integer(2): 6
    Sum of current and previous input(s) is:  13
    Input an integer(3): g
    Input an integer(3): 
    Input an integer(3): None
    Input an integer(3): True
    Input an integer(3): 4
    Sum of current and previous input(s) is:  17
    Input an integer(4): 3
    Sum of current and previous input(s) is:  20
    Input an integer(5): 7
    Sum of current and previous input(s) is:  27
    --------------------------------------------------------------------------------------------

--- SummingUpFiveUserInputNumbers.py
+++ SummingUpFiveUserInputNumbers.py
@@ -1,8 +1,12 @@
 a = 0
 x = 1
 while(x<=5):
-    n = int(input("Input num(" + str(x) +  "): "))
-    a += n    
+    try:
+        n = int(input("Input an integer(" + str(x) +  "): "))
+    except ValueError:
+        continue
+    else:
+        a += n    
     print("Sum of current and previous input(s) is: ", a)
         
     x += 1
