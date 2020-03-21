9305c37c0346a098eae9e92c5902b6845f42dfc9
Linson Joseph <linsonjosephDev@gmail.com>
2020-03-21 08:50:23 +0530

    Capitalize the initials of a name entered
    Sample out put of the program is given below:
    -----------------------------------------------------------------------------------
    Please enter your name: joseph martin
    Your name formatted with capitals is:  Joseph Martin 
    
    Do you want to enter another name (Y/(N or other))? y
    Please enter your name: niger rogger tiger wooger blogger
    Your name formatted with capitals is:  Niger Rogger Tiger Wooger Blogger 
    
    Do you want to enter another name (Y/(N or other))? n
    -----------------------------------------------------------------------------------

--- Py_CapitalizeNameInput.py
+++ Py_CapitalizeNameInput.py
@@ -14,5 +14,3 @@ while(re_enter == 'Y' or re_enter == 'y'):
     print("Your name formatted with capitals is: ", name_capitalized)
     name_capitalized = ''
     re_enter = input("\nDo you want to enter another name (Y/(N or other))? ")
-    
-
